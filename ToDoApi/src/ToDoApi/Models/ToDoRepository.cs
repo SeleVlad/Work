1d402f14027003c2ff8e7f5a8dc248c62d112796
SeleVlad <selevlad@gmail.com>
2016-10-15 01:16:47 +0300

    ToDoApi

---
+++ ToDoApi/src/ToDoApi/Models/ToDoRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace ToDoApi.Models
+{
+    public class ToDoRepository : IToDoRepository
+    {
+
+        private static ConcurrentDictionary<string, ToDoItem> _todos =
+            new ConcurrentDictionary<string, ToDoItem>();
+
+        public ToDoRepository()
+        {
+            Add(new ToDoItem { Name = "Item1" });
+        }
+
+        public IEnumerable<ToDoItem> GetAll()
+        {
+            return _todos.Values;
+        }
+
+        public void Add(ToDoItem item)
+        {
+            item.Key = Guid.NewGuid().ToString();
+            _todos[item.Key] = item;
+        }
+
+        public ToDoItem Find(string key)
+        {
+            ToDoItem item;
+            _todos.TryGetValue(key, out item);
+            return item;
+           
+        }
+
+        public ToDoItem Remove(string key)
+        {
+            ToDoItem item;
+            _todos.TryRemove(key, out item);
+            return item;
+        }
+
+        public void Update(ToDoItem item)
+        {
+            _todos[item.Key] = item;
+        }
+    }
+}
